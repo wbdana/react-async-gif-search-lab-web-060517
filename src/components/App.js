bb9d3dfe078721d999f66d362c5a0b548a8d6cc0
wbdana <william.b.dana@gmail.com>
2017-08-16 09:04:19 -0400

    functionally done, needs styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import React from 'react'
 import NavBar from './NavBar'
 import GifListContainer from '../containers/GifListContainer'
 
-// the App component should render out the GifListContainer component
-
 function App() {
   return (
     <div>
